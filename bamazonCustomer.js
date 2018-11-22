06c28ca4d50068f94aedf57e82e45291f0ffcaf7
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2018-11-21 16:43:45 -0800

    Starting work on code to display products from database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,34 @@
 // Running this application will first display all of the items available for sale.
 
 // Include: ids, names, prices of products for sale.
+
+// When the user enters, [ Node bamazonCustomer.js ] the application will display (with CLI-table) all items available for sale from the mySQL database.
+
+var mysql = require("mysql");
+var Table = require("cli-table");
+var inquirer = require("inquirer");
+
+var newLine = "\n--------------------------------------------------\n";
+var newDblLine = "\n==================================================\n";
+
+// Create initial connection for the server.
+var connection = mysql.createConnection({
+    host: "localhost",
+    post: 8080,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log(newDblLine + "Connected as id: " + connection.threadId + newDblLine);
+    displayProducts();
+    connection.end();
+});
+
+// Functions ////
+function displayProducts() {
+    console.log("Current products available in the Bamazon Catalog:" + newLine);
+}
+
