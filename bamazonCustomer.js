c402c49584ae922c447f0a9abd0392ef23bc680f
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2018-11-22 15:30:35 -0800

    Created bamazonseed.sql and finshed first part of project (basic customer interface)

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,6 +12,11 @@ var newLine = "\n---------------------------------------------------------------
 var newDblLine = "\n======================================================================\n";
 var productID = "";
 var orderQty = "";
+var currentQty = "";
+var currentProduct = "";
+var currentPID = "";
+var updatedQty = "";
+var userChoice = "";
 
 // Create initial connection for the server.
 var connection = mysql.createConnection({
@@ -24,7 +29,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log(newDblLine + "Connected as id: " + connection.threadId + newDblLine);
+    console.log(newDblLine + " Connected as id: " + connection.threadId + newDblLine);
     // Call the function to display all the products in the Bamazon Catalog.
     displayProducts();
 });
@@ -33,7 +38,7 @@ connection.connect(function(err) {
 
 // function to display products in the Bamazon MySQL Database.
 function displayProducts() {
-    console.log("Current products available in the Bamazon Catalog:" + newLine);
+    console.log(" Current products available in the Bamazon Catalog:" + newLine);
     // Instantiate a New Table.
     var table = new Table({
         head: ["Item ID#", "Product Name", "Price"],
@@ -83,7 +88,7 @@ function orderPrompt() {
 
 // Function that checks the quantity of the chosen products in the database and then orders the products and adjusts the appropriate quantities.
 function orderProducts() {
-    console.log(newDblLine + "Product to be purchase: ");
+    console.log(newDblLine + " Product to be purchased: ");
 
     // Initialize a variable to create a string for a SQL Query.
     var productQuery = "SELECT * FROM products WHERE item_id=" + productID;
@@ -100,19 +105,76 @@ function orderProducts() {
         table.push(
             [res[0].item_id, res[0].product_name, "$" + res[0].price.toFixed(2)]
         );
-    console.log(table.toString() + "\nQuantity Requested: " + orderQty);
+        console.log(table.toString() + "\n Quantity Requested: " + orderQty);
 
-    // Calc to determine total amount owed.
-    var amountOwed = orderQty * res[0].price;
+        // Calc to determine total amount owed.
+        var amountOwed = orderQty * res[0].price;
 
-    // IF-Statment, IF, orderQty < Quantity of Product. THEN, adjust Quantity of product in table AND calc the total cost to be paid by the customer.
-    if (orderQty > res[0].stock_quantity) {
-        console.log(newDblLine + "Insufficient quantity available for purchase! Sorry!" + newDblLine);
-    } else {
-        console.log(newDblLine + "Thank you for your purchase! Please pay a total of: $" + amountOwed.toFixed(2) + newDblLine);
-    }
+        // assign current qty from specific product ordered to the variable currentQty.
+        currentQty = res[0].stock_quantity;
 
-    connection.end();
+        // assign the current name of the product ordered to the variable currentProduct.
+        currentProduct = res[0].product_name;
+
+        // assign the current item_id of the product ordered to the variable currentPID.
+        currentPID = res[0].item_id;
+
+        // IF-Statment, IF, orderQty < Quantity of Product. THEN, adjust Quantity of product in table AND calc the total cost to be paid by the customer.
+        if (orderQty > res[0].stock_quantity) {
+            console.log(newDblLine + " Insufficient quantity available for purchase! Sorry!" + newDblLine);
+            
+            // call the function to ask user if they would like to order anything else.
+            orderMore();
+
+        } else {
+            console.log(newDblLine + " Thank you for your purchase! Please pay a total of: $" + amountOwed.toFixed(2) + newDblLine);
+
+            // call the function to update the products in the bamazon products table.
+            updateProducts(currentPID, currentProduct, currentQty);
+        }
     });
 } /// orderProducts();
 
+// Function to update the products table to reflect the customer's purchase.
+function updateProducts(pid, name, qty) {
+    // console.log(newDblLine + " Bamazon Product Database Updated");
+
+    updatedQty = qty - orderQty;
+
+    // Initialize a variable to create a string for a SQL Query.
+    var updateQuery = "UPDATE products SET stock_quantity=" + updatedQty + " WHERE item_id=" + pid;
+
+    // SQL Query
+    connection.query(updateQuery, function(err, res) {
+        if (err) throw err;
+
+        // console.log(newLine + " Product Name: " + name + " | " + "Updated Quantity: " + updatedQty + newLine + newDblLine);
+
+        orderMore();
+    });
+} /// updateProducts();
+
+// function to prompt/ask the user if they would like to order anything else.
+function orderMore() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                name: "userChoice",
+                message: "Would you like to order something else? ",
+                default: false
+            }
+        ])
+        .then(answer => {
+            userChoice = answer.userChoice;
+
+            // IF-statement, IF user chooses YES then re-run program, IF NO then end program.
+            if (userChoice === true) {
+                console.log(newDblLine);
+                displayProducts();
+            } else {
+                console.log(newDblLine + " Thank you for using the Bamazon CLI-app, Have a great day!" + newDblLine);
+                connection.end();
+            }
+        });
+}
